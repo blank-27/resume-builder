52272df8a267184838e7982dfb43ed59e68134c6
blank-27 <lcs2019045@iiitl.ac.in>
2022-01-12 17:09:01 +0530

    signup created

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root  'home#index'
+  get   'edit'    => 'home#edit'
+  get   'login'   => 'sessions#new'
+  post  'login'   => 'sessions#create'
+  get  'signup'  => 'signup#create'
+  post  'signup'  => 'signup#new'
+  delete 'logout'  => 'sessions#destroy'
+
+  resources :profiles , only: [:update]
+  resources :educations , only: [:new]
+end
